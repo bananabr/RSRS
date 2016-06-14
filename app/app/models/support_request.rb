8c24d4ca915092e28037bec7ffd2e714a80b9288
Daniel Berredo <danielberredo@gmail.com>
2016-06-14 14:20:10 -0300

    support request basic tests working

--- app/app/models/support_request.rb
+++ app/app/models/support_request.rb
@@ -1,6 +1,19 @@
 class SupportRequest < ActiveRecord::Base
   belongs_to :user
+
+  scope :expired, -> { where(expired: true) }
+
+  validates :user, :presence => true
+  validates :provider, :presence => true
   validates :justification, :presence => true
   validates :ttl, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: Settings.default_request_ttl.to_i }
-  validates :shared_key, :presence => true
+  validates :shared_key, :presence => true, length: {is: 32}
+
+  def expired?()
+	  if self.expired
+	    true
+	  else
+	    (self.created_at + self.ttl.minutes) <= DateTime.now
+	  end
+  end
 end
--- app/spec/models/suport_request_spec.rb
+++ app/spec/models/suport_request_spec.rb
@@ -5,17 +5,56 @@ RSpec.describe SupportRequest, type: :model do
   it "should have a justification" do
     check_field_requirement(described_class, "justification")
   end
-  it "should have a shared key" do
+
+  it "should have a shared key with exactly 32 characters" do
     check_field_requirement(described_class, "shared_key")
+    r1 = FactoryGirl.build(:support_request)
+    r2 = FactoryGirl.build(:support_request)
+    r3 = FactoryGirl.build(:support_request)
+
+    r1.shared_key = "a"*Random.rand(32)
+    r2.shared_key = "a"*32
+    r3.shared_key = "a"*33+"b"*Random.rand(100)
+
+    expect(r1).to be_invalid
+    expect(r1.errors[:shared_key]).to be_present
+    expect(r2).to be_valid
+    expect(r3).to be_invalid
+    expect(r3.errors[:shared_key]).to be_present
+
   end
+
+  it "should have a tunnel provider" do
+    check_field_requirement(described_class, "provider")
+  end
+
   it "should have a expiration time span (TTL)" do
     check_field_requirement(described_class, "ttl")
   end
-  it "should not have a expiration time span (TTL) smaller than default_request_ttl" do
+
+  it "should belong to a user" do
+    check_field_requirement(described_class, "user")
+  end
+
+  it "should not have a expiration time span (TTL) smaller than the default request ttl setting" do
     r = FactoryGirl.build(:support_request)
     r.ttl = Settings.default_request_ttl - 1
     expect(r).to be_invalid
     expect(r.errors[:ttl]).to be_present
+  end
+
+  it "should be considered expired only when TTL minutes have passed since its creation time" do
+    r1 = FactoryGirl.build(:support_request)
+    r2 = FactoryGirl.build(:support_request)
+    
+    r1.ttl = Settings.default_request_ttl + Random.rand(1000)
+    r1.ttl = Settings.default_request_ttl
+    
+    r1.created_at = DateTime.now - r1.ttl
+    r2.created_at = DateTime.now
+
+    expect(r1.expired?).to be true
+    expect(r2.expired?).to be false
   end
 
 end
